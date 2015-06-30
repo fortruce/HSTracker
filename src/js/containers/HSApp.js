3e7ab48a81908e2d3d1042095cb5dd48374aa963
Joseph Rollins <rollins.joseph@gmail.com>
2015-06-29 20:38:18 -0400

    extend Component

--- src/js/containers/HSApp.js
+++ src/js/containers/HSApp.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { Connector } from 'redux/react';
 import HSStatus from '../components/HSStatus';
 import * as HSStatusActions from '../actions/HSStatusActions';
 
-export default class HSApp {
+export default class HSApp extends Component {
 	render() {
 		return (
 			<Connector>
